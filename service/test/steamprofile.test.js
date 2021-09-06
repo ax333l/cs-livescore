9bf03be1493df5d6054282cbec5dc6092c2d1189
ax333l <themccallfree@gmail.com>
2021-09-07 00:00:52 +0300

    add test to steamprofile function

---
+++ service/test/steamprofile.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment node
+ */
+
+const getSteamMiniProfile = require('./src/steamprofile')
+
+
+describe('get mini profile', () => {
+    it('mini steam profile link', async () => {
+        const link = await getSteamMiniProfile('http://steamcommunity.com/id/alecboi')
+        expect(link).toEqual('100844742')
+    });
+
+    it('steam profile link error', async () => {
+        expect(getSteamMiniProfile('http://steam.com/fasfas/fsafsa')).rejects.toEqual('Wrong link');
+    })
+})
