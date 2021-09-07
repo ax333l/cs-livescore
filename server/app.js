1a6f645130492537e3c8299dbc6b82869944d7cc
ax333l <themccallfree@gmail.com>
2021-09-07 18:28:02 +0300

    update cors from localhost to all

--- server/app.js
+++ server/app.js
@@ -6,35 +6,52 @@ const server = Fastify({logger: true})
 
 server.register(cors, { 
     origin: (origin, cb) => {
-        if(/localhost/.test(origin)){
-            //  Request from localhost will pass
-            cb(null, true)
-            return
-        }
-        // Generate an error on other origins, disabling access
-        cb(new Error("Not allowed"))
+        cb(null, true)
+        return
     }
 })
 
 server.get('/miniprofile', async (req, res) => {
-    let {
-        body
-    } = await request(`http://localhost:3001/miniprofile?link=${req.query?.link}`)
-    body = await body.json()
-    return { body }
+    try{
+        let {
+            body
+        } = await request(`http://service:3001/miniprofile?link=${req.query?.link}`, {
+            connections: 128,
+            pipelining: 1,
+            keepAliveTimeout: 60 * 1000,
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+        body = await body.json()
+        return { body }
+    } catch(error ) {
+        return { error }
+    }
 })
 
 server.get('/score', async (req, res) => {
-    let {
-        body
-    } = await request(`http://localhost:3001/score?link=${req.query?.link}`)
-    body = await body.json()
-    return { body }
+    try{
+        let {
+            body
+        } = await request(`http://service:3001/score?link=${req.query?.link}`, {
+            connections: 128,
+            pipelining: 1,
+            keepAliveTimeout: 60 * 1000,
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+        body = await body.json()
+        return { body }
+    } catch(error) {
+        return { error }
+    }
 })
 
 const start = async () => {
     try {
-        await server.listen(process.env.PORT || 5000)
+        await server.listen(process.env.PORT || 5000, '0.0.0.0')
 
         const address = server.server.address()
         const port = typeof address === 'string' ? address : address?.port
